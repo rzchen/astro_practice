e00dfe6055e6d610347018692be2d5b4e7967dd7
rzchen <jeff820523@gmail.com>
2020-07-14 09:39:30 +0800

    rest

---
+++ ruby-courese/hw1.rb
@@ -0,0 +1,36 @@
+# class Array
+#   def my_map
+#     a = []
+#     self.each do |num|
+#       a << yield(num)
+#     end
+#     a
+#   end  
+# end
+
+# p [1, 2, 3, 4, 5].my_map { |x| x * 2 }  
+# # 印出 [2, 4, 6, 8, 10]
+
+
+class String
+  def is_email?
+    if self =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      true
+    elsif
+      false
+    end
+  end
+end
+
+
+
+
+
+p "yuu@5xruby.tw".is_email?             
+# 印出 true
+
+p "yuu.kk@5xruby.tw".is_email?          
+# 印出 true
+
+p "what".is_email?                      
+# 印出 false
