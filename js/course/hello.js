f53d1cc0486214887452df5b13418549ead0de7c
rzchen <jeff820523@gmail.com>
2020-08-05 12:35:06 +0800

    js var let f

---
+++ js/course/hello.js
@@ -0,0 +1,48 @@
+// let x = 10;
+// let y = "100";
+
+// // let userName = "John";
+// // let greeting = "Hello, " + userName + ", how are you.";
+// // let greeting2 = `Hello, ${userName}, how are you.`;
+
+// x = 999;
+// y = `test`; // grave, backtick
+
+// let z = x + y;
+// // console.log(z);
+
+// function foo() {
+//   x1 = 100;
+// }
+// foo();
+// console.log(x1);
+
+// // scope 變數作用域
+// // global scope 全域變數
+// // function scope 函式內區域變數
+// // block scope 區塊內區域變數 ->
+
+// for (let i = 0; i < 10; i++) {
+// }
+
+// // console.log(i);
+
+// function bar(x) {
+//   for (var j = 0; j < x; j++) {
+//     // 1. 拿得到 j
+//   }
+//   // 2. 拿不到 j
+// }
+// // 3. 拿不到 j
+
+// // j++ 
+// // j = j + 1
+// // j += 1
+
+console.log(userName);
+
+let userName = "John Doe" // Camel case
+
+console.log(userName);
+
+
