68c67ed7eaabb529959ad3f186650842fbac65d8
rzchen <jeff820523@gmail.com>
2020-08-14 10:22:06 +0800

    js

---
+++ js/course/0813-js.js
@@ -0,0 +1,47 @@
+// function foo(x, y, z) {
+//   console.log("x: ", x);
+//   console.log("y: ", y);
+//   console.log("z: ", z);
+// }
+
+// // foo(1, 2, 3)
+// // foo(1, 2, 3, 4, 5, 6)
+// foo(1, 2)
+
+// let [head, ...tails] = ary
+// console.log(head)
+// console.log(tails)
+
+// let student = {name: 'john', age: 13, gender: 'M'}
+// // let studentName = student.name
+// // let studentAge = student.age
+// let 
+
+// for(let lang of langs) {
+//   console.log
+// }
+
+
+// let pricesByCountry = {
+//   'tw' : 300,
+//   'us' : 10.2,
+//   'jp' : 1250
+// }
+
+// let result = Object.entries(pricesByCountry)
+//                    .map(tpl => [tpl[0], tpl[1] * 1.05])
+
+// let newPrice = Object.fromEntries(result)
+// console.log(newPrice)
+
+let names = ['apple', 'banana', 'cherry'];
+let fruits1 = ['watermelon', 'pear']
+let allFruits = names.concat(fruits1);
+console.log(allFruits);
+console.log(names);
+console.log(fruits1);
+
+
+
+
+
